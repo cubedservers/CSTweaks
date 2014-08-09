2589e8ad1f01ad3fd222697e2a1b8061300324ae
dariusc93 <jesus4us7>
2014-08-09 13:57:58 -0400

    Gradle script and main class

---
+++ src/main/java/org/cubedservers/cstweaks/CSTweaks.java
@@ -0,0 +1,31 @@
+package org.cubedservers.cstweaks;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(name="CSTweaks", modid="cstweaks", version="0")
+public class CSTweaks {
+
+    @Instance
+    public static CSTweaks instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void serverStarting(FMLServerStartingEvent event){
+        
+    }
+
+}
