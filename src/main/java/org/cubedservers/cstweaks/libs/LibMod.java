6286e8114178a16cedb8316d32b779f5b2ef601b
wasliebob <wasliebob@live.nl>
2014-08-09 20:30:33 +0200

    Some main work on stuff, cleaning it up

---
+++ src/main/java/org/cubedservers/cstweaks/libs/LibMod.java
@@ -0,0 +1,8 @@
+package org.cubedservers.cstweaks.libs;
+
+public class LibMod {
+	public static final String version = "0";
+	public static final String mcVersion = "1.7.10";
+	public static final String modName = "CSTweaks";
+	public static final String modId = "cstweaks";
+}
--- src/main/java/org/cubedservers/cstweaks/main/CSTweaks.java
+++ src/main/java/org/cubedservers/cstweaks/main/CSTweaks.java
@@ -1,13 +1,15 @@
 package org.cubedservers.cstweaks.main;
 
+import org.cubedservers.cstweaks.libs.LibMod;
+
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(name="CSTweaks", modid="cstweaks", version="0")
+@Mod(name=LibMod.modName, modid=LibMod.modId, version=LibMod.version)
 public class CSTweaks {
 
     @Instance
